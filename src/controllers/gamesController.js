b0dd0bcce8bfcf1fce70e93d6a1f4fbb441ac226
C-Macargo <caio.camargos97@gmail.com>
2023-02-09 10:07:59 -0300

    feat, post games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,11 +1,25 @@
 import { db } from "../configs/database.connection.js";
 
 export async function listGames(_, res) {
-    try {
-      const games = await db.query('SELECT * FROM games');
-      res.send(games.rows);
-    } catch (error) {
-      res.sendStatus(500);
-    }
-  }
+	try {
+		const games = await db.query("SELECT * FROM games");
+		res.send(games.rows);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+}
 
+export async function createGame(req, res) {
+	const { name, image, stockTotal, pricePerDay } = req.body;
+	try {
+		const game = await db.query(
+			`INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1, $2, $3, $4) RETURNING *`,
+			[name, image, stockTotal, pricePerDay]
+		);
+		console.log(game.rows[0]);
+		res.status(201).send("game added successfully");
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send(err.message);
+	}
+}
--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import {listGames} from "../controllers/gamesController.js"
+import {listGames, createGame} from "../controllers/gamesController.js"
+import { createGameSchema } from '../schema/createGameSchema.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
 
 const gamesRouter = Router();
 
 gamesRouter.get('/games', listGames);
+gamesRouter.post('/games',validateSchema(createGameSchema),  createGame);
+
 
 export default gamesRouter;
