0b4caef668867bf167ea938ba5c08600d5c20ff4
C-Macargo <caio.camargos97@gmail.com>
2023-02-09 19:27:58 -0300

    fix, schema syntax

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { listGames, createGame } from "../controllers/gamesController.js";
-import { createGameSchema } from "../schema/createGameSchema.js";
+import { gameSchema } from "../schema/gameSchema.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", listGames);
-gamesRouter.post("/games", validateSchema(createGameSchema), createGame);
+gamesRouter.post("/games", validateSchema(gameSchema), createGame);
 
 export default gamesRouter;
--- src/schema/gameSchema.js
+++ src/schema/gameSchema.js
@@ -1,6 +1,6 @@
 import joi from 'joi'
 
-export const createGameSchema = joi.object ({
+export const gameSchema = joi.object ({
     name: joi.string().required(),
     image: joi.string().required(),
     stockTotal: joi.number().required().positive(),
