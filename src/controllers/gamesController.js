cc234889a35d700439f40e5ca4e485dd89a4aca7
C-Macargo <caio.camargos97@gmail.com>
2023-02-09 11:02:06 -0300

    fix, adressing duplicates + additional joi validations

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -12,6 +12,13 @@ export async function listGames(_, res) {
 export async function createGame(req, res) {
 	const { name, image, stockTotal, pricePerDay } = req.body;
 	try {
+		const existingGame = await db.query(
+			`SELECT * FROM games WHERE name = $1`,
+			[name]
+		);
+		if (existingGame.rows.length > 0) {
+			return res.status(409).send("Game already exists");
+		}
 		const game = await db.query(
 			`INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1, $2, $3, $4) RETURNING *`,
 			[name, image, stockTotal, pricePerDay]
--- src/schema/createGameSchema.js
+++ src/schema/createGameSchema.js
@@ -3,6 +3,6 @@ import joi from 'joi'
 export const createGameSchema = joi.object ({
     name: joi.string().required(),
     image: joi.string().required(),
-    stockTotal: joi.number().required(),
-    pricePerDay: joi.number().required(),
+    stockTotal: joi.number().required().positive(),
+    pricePerDay: joi.number().required().positive(),
 })
