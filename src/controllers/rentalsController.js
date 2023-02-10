2bc85c6a246bb3039d5043a2134e09d51048485c
C-Macargo <caio.camargos97@gmail.com>
2023-02-10 12:13:59 -0300

    feat, post rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { db } from "../configs/database.connection.js";
 
 export async function listRentals(_, res) {
@@ -5,7 +6,48 @@ export async function listRentals(_, res) {
 		const rentals = await db.query("SELECT * FROM rentals");
 		res.send(rentals.rows);
 	} catch (error) {
-		res.sendStatus(500);
+		return res.status(500).send(err.message);
 	}
 }
 
+export async function createRental(req, res) {
+	const { customerId, gameId, daysRented } = req.body;
+	const rentalDate = dayjs(Date.now()).format("YYYY-MM-DD");
+
+	try {
+		const customerInformation = await db.query(
+			"SELECT * FROM customers WHERE id = $1",
+			[customerId]
+		);
+		const gameInformation = await db.query(
+			"SELECT * FROM games WHERE id = $1",
+			[gameId]
+		);
+		if (
+			customerInformation.rowCount === 0 ||
+			gameInformation.rowCount === 0
+		) {
+			return res.status(400).send("user or game do not exist");
+		}
+		const { pricePerDay } = gameInformation.rows[0];
+		const originalPrice = pricePerDay * daysRented;
+
+		await db.query(
+			`INSERT INTO rentals 
+            ("customerId", "gameId", "rentDate", "daysRented", "returnDate","originalPrice", "delayFee") 
+            VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+			[
+				customerId,
+				gameId,
+				rentalDate,
+				daysRented,
+				null,
+				originalPrice,
+				null,
+			]
+		);
+		return res.status(201).send("rental created successfully");
+	} catch (err) {
+		return res.status(500).send(err.message);
+	}
+}
--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { listRentals } from "../controllers/rentalsController.js";
+import { listRentals, createRental } from "../controllers/rentalsController.js";
+import { rentalSchema } from "../schema/rentalSchema.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", listRentals);
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), createRental );
 
 export default rentalsRouter;
